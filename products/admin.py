42c2b08fe14a845a1ec3c8eca27c820b67388b25
Uddhika <uddhikaishara@gmail.com>
2021-12-29 11:52:59 +0530

    first commit

---
+++ products/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.db import models
+from .models import Product,Offer
+
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display=('code','description','discount')
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name','price','stock','image_url')
+
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
+
